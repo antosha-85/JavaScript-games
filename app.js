d66f8c3544c5d3c8931a9431067e16c09c40789b
Anton Smirnov <antosha-85@yandex.ru>
2020-04-13 00:08:15 -0600

    Finished memory game

--- app.js
+++ app.js
@@ -51,21 +51,60 @@ document.addEventListener('DOMContentLoaded', ()=> {
         }
 
     ]
+    
+    cardArray.sort(()=> 0.5 - Math.random())
 
-    const grid = document.querySelector('.grid')
+    const grid = document.querySelector('.grid');
+    var resultDisplay = document.querySelector('#result')
+    var cardsChosen = [];
+    var cardsChosenId = [];
+    var cardsWon = [];
     //create your board
-    createBoard = () => {
+    function createBoard () {
         for (let i = 0; i < cardArray.length; i++) {
-            const card = document.createElement('img');
+            var card = document.createElement('img');
             card.setAttribute('src', './images/blank.png');
             card.setAttribute('data-id', i);
+            card.addEventListener('click', flipCard)
             grid.appendChild(card);
         }
     }
-    createBoard();
-
+    
     //check for matches
-
+    
+    function checkForMatch () {
+        var cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0];
+        const optionTwoId = cardsChosenId[1];
+        if (cardsChosen[0] === cardsChosen[1]) {
+            alert('You found a match')
+            cards[optionOneId].setAttribute('src', './images/white.png')
+            cards[optionTwoId].setAttribute('src', './images/white.png')
+            cardsWon.push(cardsChosen);
+        } else {
+            cards[optionOneId].setAttribute('src', './images/blank.png')
+            cards[optionTwoId].setAttribute('src', './images/blank.png')
+            alert('Sorry try again')
+        }
+        cardsChosen = [];
+        cardsChosenId = [];
+        resultDisplay.textContent = cardsWon.length;
+        if (cardsWon.length === cardArray.length/2) {
+            resultDisplay.textContent = 'Congratulations! You found them all!'
+        }
+    }
     //flip your card
     
+    function flipCard () {
+        var cardId = this.getAttribute('data-id');
+        // if (cardsChosenId.length ===1 && cardsChosenId[0]==cardId){return}
+        // if(this.src.search('white.png')!=-1){return}
+        cardsChosen.push(cardArray[cardId].name);
+        cardsChosenId.push(cardId);
+        this.setAttribute('src', cardArray[cardId].img)
+        if(cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500) 
+        }
+    }
+    createBoard();
 })
